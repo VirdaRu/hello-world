7b1042c7216b52a27f8ecd7651990549ef5cdd4a
yPinguin <timsmittak@gmail.com>
2019-10-29 14:09:33 +0100

    memory project v1.6.1

---
+++ memory project v1.6.1/MemoryGame/MemoryGame/MemoryGrid.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Input;
+
+namespace MemoryGame
+{
+    class MemoryGrid
+    {
+        private Grid grid;
+        private int cols;
+        private int rows;
+            
+
+        public MemoryGrid(Grid grid, int cols, int rows)
+        {
+            this.grid = grid;
+            this.cols = cols;
+            this.rows = rows;
+            InitializeGameGrid(cols, rows);
+            AddImages();
+        }
+
+        private void InitializeGameGrid(int cols, int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < cols; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+        private void AddImages()
+            //Dit is de voorkant van de kaartjes (Lars)
+        {
+            List<ImageSource> images = GetImagesList();
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < cols; column++)
+                {
+                    Image backgroundImage = new Image();
+                    backgroundImage.Source = new BitmapImage(new Uri("img/sesame.png", UriKind.Relative));
+                    backgroundImage.Tag = images.First();
+                    images.RemoveAt(0);
+                    backgroundImage.MouseDown += new MouseButtonEventHandler(CardClick);
+                    Grid.SetColumn(backgroundImage, column);
+                    Grid.SetRow(backgroundImage, row);
+                    grid.Children.Add(backgroundImage);
+                }
+            }
+        }
+        //plaatjes draaien (lars)
+        private void CardClick(object sender, MouseButtonEventArgs e)
+        {
+            Image card = (Image)sender;
+            ImageSource front = (ImageSource)card.Tag;
+            card.Source = front;
+        }
+        //Dit zijn de plaatjes van de memory (Lars, Max)
+        private List<ImageSource> GetImagesList()
+        {
+            List<ImageSource> images = new List<ImageSource>();
+            for (int i = 0; i < 16; i++)
+            {
+                int imageNr = i % 8 + 1;
+                ImageSource source = new BitmapImage(new Uri("img/" + imageNr + ".png", UriKind.Relative));
+                images.Add(source);
+            }
+            //dit is de randomizer  (Rianne, Max)
+                Random random = new Random();
+                for (int i = 0; i < (rows * cols); i++)
+                {
+                int r = random.Next(0, (rows * cols));
+                ImageSource randomnaam = images[r];
+                images[r] = images[i];
+                images[i] = randomnaam;
+                }
+            return images;
+        }
+
+    }
+
+}
---
+++ memory project v1.6.1/MemoryGame/MemoryGame/Page3.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryGame
+{
+    /// <summary>
+    /// Interaction logic for Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        private const int NR_OF_COLS = 4;
+        private const int NR_OF_ROWS = 4;
+        private MemoryGrid grid;
+        public Page3()
+        {
+            InitializeComponent();
+            grid = new MemoryGrid(GameGrid, NR_OF_COLS, NR_OF_ROWS);
+            InitializeGameGrid();
+        }
+
+        private void InitializeGameGrid()
+        {
+            for (int i = 0; i < NR_OF_ROWS; i++)
+            {
+                GameGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < NR_OF_COLS; i++)
+            {
+                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+    }
+}
