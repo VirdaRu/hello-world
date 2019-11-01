94bbf5b9b29f75572e14a26aba7eea59979e162f
maksmink <maxmink@home.nl>
2019-11-01 13:33:46 +0100

    v1.8

---
+++ memory project v1.8/MemoryGame/MemoryGame/Page1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MemoryGame
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+         
+        private void DootDoot_CLick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page2());
+        }
+        private void Click_CLick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new highscore());
+        }
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page4());
+        }
+       
+
+    }
+}
---
+++ memory project v1.8/MemoryGame/MemoryGame/Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryGame
+{
+    /// <summary>
+    /// Interaction logic for highscore.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+
+        }
+
+        public List<string> playernames = new List<string>();
+        private void SpelenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            playernames.Add(naam1.Text);
+            playernames.Add(naam2.Text);
+
+            this.NavigationService.Navigate(new Page3(playernames));
+        }
+
+
+
+
+        private void back_CLick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page1());
+        }
+
+    }
+}
