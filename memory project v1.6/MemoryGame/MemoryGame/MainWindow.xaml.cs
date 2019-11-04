6360932ecbf79e2e70e2f15aabe16bf8bb402795
maksmink <maxmink@home.nl>
2019-11-04 18:49:55 +0100

    alle dingen

---
+++ memory project v1.6/MemoryGame/MemoryGame/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MemoryGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MyWindow_Loaded;
+        }
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Frame.NavigationService.Navigate(new Page1());
+        }
+
+    }
+}
+
